26484086a4c924246edf03b3583413b4a665e3eb
Adam MIller <adammiller@Adams-MBP.domain>
2018-02-25 20:13:51 -0700

    all burger files uploaded

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Blu Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Southwestern Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black and Blue Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Threat Chili Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Charged Chicken Swiss Burger', true);
